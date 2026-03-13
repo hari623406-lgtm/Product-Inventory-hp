a0a469524b8095016ce38793e8d533f70a54a553
Hariprakash <hari623406@gmail.com>
2026-03-13 19:33:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,17 @@
 'use strict';
 
-var isGlob = require('is-glob');
-var pathPosixDirname = require('path').posix.dirname;
-var isWin32 = require('os').platform() === 'win32';
-
-var slash = '/';
-var backslash = /\\/g;
-var enclosure = /[\{\[].*[\}\]]$/;
-var globby = /(^|[^\\])([\{\[]|\([^\)]+$)/;
-var escaped = /\\([\!\*\?\|\[\]\(\)\{\}])/g;
-
-/**
- * @param {string} str
- * @param {Object} opts
- * @param {boolean} [opts.flipBackslashes=true]
- * @returns {string}
+/*!
+ * Module dependencies.
  */
-module.exports = function globParent(str, opts) {
-  var options = Object.assign({ flipBackslashes: true }, opts);
 
-  // flip windows path separators
-  if (options.flipBackslashes && isWin32 && str.indexOf(slash) < 0) {
-    str = str.replace(backslash, slash);
-  }
+const mongodbDriver = require('./drivers/node-mongodb-native');
 
-  // special case for strings ending in enclosure containing path separator
-  if (enclosure.test(str)) {
-    str += slash;
-  }
+require('./driver').set(mongodbDriver);
 
-  // preserves full path in case of trailing path separator
-  str += 'a';
+const mongoose = require('./mongoose');
 
-  // remove path parts that are globby
-  do {
-    str = pathPosixDirname(str);
-  } while (isGlob(str) || globby.test(str));
+mongoose.setDriver(mongodbDriver);
 
-  // remove escape chars and return result
-  return str.replace(escaped, '$1');
-};
+mongoose.Mongoose.prototype.mongo = require('mongodb');
+
+module.exports = mongoose;
